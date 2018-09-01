35cb1f0f9b3c42b519e024c299063bb647021b94
Rafael <bla>
2018-09-01 19:58:42 +0200

    Test

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+
+
+        newArray liste = new newArray();
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+    //public static Collection<String> removeEmptyWords( String s ) {
+
+        //String [] wörter = s.split(" ");
+        //for ( int i = 0; i < wörter.length; i++ ) {
+            //String wort = wörter[ i ];
+
+
+
+
+
+
---
+++ src/newArray.java
@@ -0,0 +1,27 @@
+public class newArray {
+
+    String s = "Wald ist überflüssig. Auf anderen Planeten gibt es auch keine Bäume.";
+
+    //String [] tokens = s.split( " " );
+    //for ( int i = 0; i < tokens.length; i++ ) {
+    //String token = tokens[ i ];
+    //System.out.println(token);
+    String[] = ("der", "das", "er", "sie", "ein", "eine", "keine", "auf")
+public static void bern{
+
+        String[] wörter = s.split(" ");
+        for (int i = 0; i < wörter.length; i++) {
+            String wort = wörter[i];
+            System.out.println(wort);
+
+        }
+
+        for (int i = 0; i < wörter.length; i++) {
+
+
+
+        }
+
+
+    }
+}
